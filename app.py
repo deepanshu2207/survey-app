5691793181c489c05fd50fecfdb7e5c663786ab1
deepanshu2207 <62646089+deepanshu2207@users.noreply.github.com>
2020-08-25 12:29:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1014,7 +1014,8 @@ def selectedque_q3(selectedque):
     return jsonify(lst)                
 
 
-
-app.run(port=5000,debug=True) 
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
+ 
 
 
